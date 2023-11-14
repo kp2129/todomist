d2f8ba55063d8062ae71572b2c3140e96018b3f4
kp2129 <kristapspolis0@gmail.com>
2023-11-14 12:47:52 +0200

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css';
+import './style/index.css';
 import Navbar from './components/navbar';
 
 export default function App() {
--- frontend/src/components/pages/projects.js
+++ frontend/src/components/pages/projects.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../index.css';
+import '../style/index.css';
 
 function projects() {
   return (
