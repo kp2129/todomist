283ae59337d8d70db72ca99780ad911406f33706
Leons Aleksandrovs <skrazzo@proton.me>
2023-11-29 15:24:00 +0200

    calendar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,15 @@ import React from 'react';
 import './style/index.css';
 import View from "./components/View"
 import Aside from './components/aside';
+
 import Navbar from './components/navbar';
 import Backlog from './components/pages/backlog';
+<<<<<<< Updated upstream
+=======
+import Calendar from './components/pages/Calendar';
+import NotFound from './components/pages/NotFound';
+import View from './components/View';
+>>>>>>> Stashed changes
 
 export default function App() {
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import App from './App';
+import './style/scrollbars.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
