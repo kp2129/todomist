4f19874054e1e8ac01f3156bc714b69b9733df5c
Silvestrs <SilvestrsL47@gmail.com>
2023-12-13 10:08:55 +0200

    Added remainder.

--- frontend/src/components/pages/reminder.js
+++ frontend/src/components/pages/reminder.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
-import React from 'react'
+// import React from 'react'
 
-const reminder = () => {
+const Reminder = () => {
 
     const [reminders, setReminders] = useState([]);
 
@@ -17,24 +17,30 @@ const reminder = () => {
             }else{
                 let test = Object.entries(res.data);
                 let tempData = [];
-                let issues = [];
                 for(let i = 0; i < test.length; i++){
-                    let start_year = new Date(test[i][1]["updated_at"]).getFullYear();
-                    let start_month = new Date(test[i][1]["updated_at"]).getMonth()+1;
-                    let start_date = new Date(test[i][1]["updated_at"]).getDate();
+                    let current_year = new Date().getFullYear();
+                    let current_month = new Date().getMonth()+1;
+                    let current_date = new Date().getDate();
                     let end_year = new Date(test[i][1]["dueDate"]).getFullYear();
                     let end_month = new Date(test[i][1]["dueDate"]).getMonth()+1;
                     let end_date = new Date(test[i][1]["dueDate"]).getDate();
                     // console.log(start_date);
-                    tempData.push({ name: `${test[i][1]["taskName"]}`, endDate: `${end_date}/${end_month}/${end_year}`});
+                    if(end_date - current_date >= 0 && end_month - current_month === 0 && end_year - current_year === 0){
+                        tempData.push({ name: `${test[i][1]["taskName"]}`, endDate: `${end_date}/${end_month}/${end_year}`, days: end_date-current_date});
+                    }
                 } 
                 setReminders(tempData);
             }
         });
     }, []);
   return (
-    <div>reminder</div>
+    <>
+        {reminders.map((sprint) => (
+            <h3 className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded font-bold text-xl'>{sprint.name} is due on {sprint.endDate} in {sprint.days} days.</h3>
+            // <p>is due on</p> 
+        ))}
+    </>
   )
 }
 
-export default reminder
+export default Reminder
